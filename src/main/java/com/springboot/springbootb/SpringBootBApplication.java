923d3b1d3de14e47d79b5c22cfeb1cc4bdd8067f
saba-calin <sabaila.calin10@gmail.com>
2025-03-11 15:51:41 -0700

    Initial commit

---
+++ src/main/java/com/springboot/springbootb/SpringBootBApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBApplication.class, args);
+    }
+
+}
